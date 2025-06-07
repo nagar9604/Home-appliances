588bf65054c23a6519522a97ce70038d9f192b54
shruti <nagarshruti96@gmail.com>
2025-06-07 22:27:16 +0530

    homeapp

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+//https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import history from 'connect-history-api-fallback'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    middlewareMode: false, // important: false for normal mode
+    setupMiddlewares(middlewares, devServer) {
+      middlewares.use(
+        history({
+          verbose: true,
+        })
+      )
+      return middlewares
+    }
+  }
 })
+
