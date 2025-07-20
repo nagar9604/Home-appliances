afe4195b55f179ef37a427cda97af3d223c79c60
shruti <nagarshruti96@gmail.com>
2025-07-20 14:30:28 +0530

    commit 2

--- src/FRONTEND/ADMIN/CategoryForm.jsx
+++ src/FRONTEND/ADMIN/CategoryForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 function CategoryForm({ category, fetchCategories, setSelectedCategory }) {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
